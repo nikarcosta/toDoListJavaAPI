f604036584997195da71972f566ea1c3625bfc9c
nikarcosta <mrc32467098@outlook.com>
2024-09-28 09:00:04 -0300

    feat: add bcrypt library & add hash password

--- user/UserController.java
+++ user/UserController.java
@@ -3,6 +3,9 @@ package com.nikarcosta.todolist.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +27,10 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists!");
         }
 
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
+        
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
