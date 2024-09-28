a7d33c0fff78639c02a3aa91a0615158390c1a9f
nikarcosta <mrc32467098@outlook.com>
2024-09-28 08:44:46 -0300

    feat: add IUserRepository

--- user/UserController.java
+++ user/UserController.java
@@ -3,6 +3,9 @@ package com.nikarcosta.todolist.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -10,8 +13,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequestMapping("/users")
 public class UserController {
     
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")    
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        
+        if(user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists!");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
