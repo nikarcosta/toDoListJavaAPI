dbf19df6d0f1d6dc8c35947a74aec25bdb003d2d
nikarcosta <mrc32467098@outlook.com>
2024-11-24 10:05:22 -0300

    feat: validate if the task belongs to logged user before updating it

--- task/TaskController.java
+++ task/TaskController.java
@@ -61,12 +61,24 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@PathVariable UUID id, @RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity update(@PathVariable UUID id, @RequestBody TaskModel taskModel, HttpServletRequest request) {
         var task = this.taskRepository.findById(id).orElse(null);
+
+        if(task == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada.");
+        }
+
+        var idUser = request.getAttribute("idUser");
+
+        if(!task.getIdUser().equals(idUser)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não tem permissão para alterar a tarefa.");
+        }
 
         Utils.copyNonNullProperties(taskModel, task);
 
-        return this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save((task));
+
+        return ResponseEntity.ok().body(taskUpdated);
         
     }
 }
